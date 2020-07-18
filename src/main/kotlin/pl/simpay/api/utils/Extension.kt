d2fe649e1989ff308dfb10f0d8eb417582d394f8
Kacper <kacperek1003@gmail.com>
2020-07-18 12:43:39 +0200

    Code Cleanup

--- src/main/kotlin/pl/simpay/api/utils/Extension.kt
+++ src/main/kotlin/pl/simpay/api/utils/Extension.kt
@@ -1,7 +1,6 @@
 package utils
 
 import com.google.common.hash.Hashing
-import model.db.request.DbServiceCommissionRequest
 import java.nio.charset.StandardCharsets
 import java.text.DecimalFormat
 import java.text.Normalizer
@@ -16,9 +15,4 @@ internal fun String.normalizeToNFKD(): String {
 
 internal fun Double.formatTwoDigitAfterComma(): String {
     return DecimalFormat("0.##").format(this)
-}
-
-
-fun main() {
-    DbServiceCommissionRequest()
 }
