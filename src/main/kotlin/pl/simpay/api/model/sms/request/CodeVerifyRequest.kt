f1b0eb58784c8ae536b74acacba3b8a7105bbdba
Mateusz Kucz <mati.kucz65@gmail.com>
2020-08-13 19:44:24 +0200

    Added tests for Sms.kt

--- src/main/kotlin/pl/simpay/api/model/sms/request/CodeVerifyRequest.kt
+++ src/main/kotlin/pl/simpay/api/model/sms/request/CodeVerifyRequest.kt
@@ -1,4 +1,10 @@
 package model.sms.request
 
-data class CodeVerifyRequest(var key: String, var secret: String,
-                             var service_id: String, var number: String, var code: String)
+data class CodeVerifyRequest(
+    var key: String,
+    var secret: String,
+    var service_id: String
+) {
+    lateinit var number: String
+    lateinit var code: String
+}
--- src/main/kotlin/pl/simpay/api/payments/Sms.kt
+++ src/main/kotlin/pl/simpay/api/payments/Sms.kt
@@ -1,6 +1,7 @@
 package payments
 
 import model.generic.ApiResponse
+import model.generic.ParametrizedRequest
 import model.sms.request.CodeVerifyRequest
 import model.sms.request.ServiceListRequest
 import model.sms.response.CodeVerifyResponse
@@ -14,11 +15,11 @@ class Sms {
 
     // https://docs.simpay.pl/#weryfikacja-kodu
     fun verifyCode(request: CodeVerifyRequest): ApiResponse<CodeVerifyResponse> {
-        return sendPost(VERIFY_CODE_URL, request, ApiResponse())
+        return sendPost(VERIFY_CODE_URL, ParametrizedRequest(request), ApiResponse())
     }
 
     // https://docs.simpay.pl/#pobieranie-listy-uslug
     fun getServiceList(request: ServiceListRequest): ApiResponse<ServicesResponse> {
-        return sendPost(SERVICE_LIST_URL, request, ApiResponse())
+        return sendPost(SERVICE_LIST_URL, ParametrizedRequest(request), ApiResponse())
     }
 }
